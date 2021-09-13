ba705be27e2df884c599f1228c7a8830cba2436f
Yimura <andreas.maerten@scarlet.be>
2021-09-13 16:43:13 +0200

    feat(Index): Updated lavalink to latest

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import BaseModule from './structures/BaseModule.js'
-import pkg from 'shoukaku'
+import Shoukaku, { Shoukaku as LavaLinkConnector } from 'shoukaku'
 
 export default class LavaLink extends BaseModule {
     constructor(main) {
@@ -15,17 +15,18 @@ export default class LavaLink extends BaseModule {
     }
 
     init() {
-        this._conn = new pkg.Shoukaku(this._m, this.auth.credentials.lavalink, {
+        this._conn = new LavaLinkConnector(new Shoukaku.Libraries.DiscordJS(this._m), this.auth.credentials.lavalink, {
             moveOnDisconnect: false,
-            resumable: 'lavaRetryTheRedemptionArc',
-            resumableTimeout: 30,
+            resumable: true,
+            resumableTimeout: 2e4,
             reconnectTries: 100,
-            restTimeout: 2e4
+            restTimeout: 3e4
         });
 
+        this.conn.on('debug', (name, info) => this.log.verbose('LAVALINK', info));
         this.conn.on('ready', (name) => this.log.info('LAVALINK', `Node: ${name} is now connected`));
         this.conn.on('error', (name, error) => this.log.error('LAVALINK', `Node: ${name} emitted an error.\n${error.stack}`));
-        this.conn.on('close', (name, code, reason) => this.log.warn('LAVALINK', `Node: ${name} closed with code ${code}. Reason: ${reason || 'No reason'}`));
+        this.conn.on('close', (name, code, reason) => this.log.warn('LAVALINK', `Node: ${name} closed with code ${code}. Reason: "${reason || 'No reason'}"`));
         this.conn.on('disconnected', (name, reason) => this.log.critical('LAVALINK', `Node: ${name} disconnected. Reason: ${reason || 'No reason'}`));
 
         return true;
